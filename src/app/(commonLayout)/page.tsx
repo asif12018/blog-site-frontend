bb45d53e9b3d960e69f7611d4da83e8a18dd3659
asif12018 <asif.request@gmail.com>
2026-01-28 11:59:16 +0600

    post service for data fetching

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,5 +1,6 @@
 
 import { Button } from '@/components/ui/button';
+import { blogService } from '@/services/blog.service';
 import { userService } from '@/services/user.service';
 
 
@@ -8,6 +9,8 @@ import { userService } from '@/services/user.service';
 export default async function Home() {
   // const {data, error} = await userService.getSession();
   // console.log("result:",data);
+  const {data} = await blogService.getBlogPost();
+  
   return (
     <div>
       <Button variant="outline">Click here</Button>
